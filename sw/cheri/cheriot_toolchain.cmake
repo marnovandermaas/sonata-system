4d8a90a378a6f68247f6b49f55b4c786aa97bbac
Hugo McNally <hugo.mcnally@gmail.com>
2024-05-06 19:02:55 +0100

    Moved cheri_sanity over to cmake

---
+++ sw/cheri/cheriot_toolchain.cmake
@@ -0,0 +1,27 @@
+set(LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/cheri_sanity/irom.ldscript")
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_ASM_COMPILER clang)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+
+set(CMAKE_ASM_COMPILER_TARGET riscv32-unknown-unknown)
+set(CMAKE_C_COMPILER_TARGET riscv32-unknown-unknown)
+set(CMAKE_CXX_COMPILER_TARGET riscv32-unknown-unknown)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
+
+
+string(CONCAT CMAKE_CXX_FLAGS_INIT
+  "-mcpu=cheriot -mabi=cheriot -mxcheri-rvc -mrelax -fshort-wchar"
+  " -Os -g -fomit-frame-pointer -fno-builtin -fno-exceptions"
+  " -fno-asynchronous-unwind-tables -fno-c++-static-destructors -fno-rtti"
+  " -Wall -Werror -fvisibility=hidden -fvisibility-inlines-hidden"
+)
+
+
+set(CMAKE_ASM_FLAGS_INIT "-mcpu=cheriot -mabi=cheriot -mxcheri-rvc -mrelax -fshort-wchar")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-nodefaultlibs -fuse-ld=lld -T \"${LINKER_SCRIPT}\"")
